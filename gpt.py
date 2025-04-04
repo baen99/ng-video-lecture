b3584c9efe69ebb4da437841775c8c0d169851e4
Ben <161524646+baen99@users.noreply.github.com>
2025-04-04 13:09:30 +0200

    Update gpt.py
    reading in path to input text from the command line

--- gpt.py
+++ gpt.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import sys
 
 # hyperparameters
 batch_size = 64 # how many independent sequences will we process in parallel?
@@ -19,7 +20,11 @@ dropout = 0.2
 torch.manual_seed(1337)
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('input.txt', 'r', encoding='utf-8') as f:
+try:
+    inp = sys.argv[1]
+except IndexError:
+    inp = 'input.txt'
+with open(inp, 'r', encoding='utf-8') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
